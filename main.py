fa8f650b40e1982a7a36ba2587ae2fb4a77a1016
BlathanAevon <90953033+BlathanAevon@users.noreply.github.com>
2023-10-03 10:16:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# This Python file uses the following encoding: utf-8
+
 import telebot
 import openai
 import threading
 import time
 import os
+import csv
+import pandas as pd
 from dotenv import load_dotenv
 from loguru import logger
 
@@ -11,35 +15,13 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
-
 def summarize(input_data):
     return openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": "Проанализируй сообщения и сделай краткую выжимку из 10-15 пунктов которые могут быть наиболее интересны аудитории чата, верни каждое сообщение гиперссылкой (используя ссылку на каждое сообщение) в формате <a href='ссылка'>сообщение</a>, оформи весь ответ красиво, если информация не подана или ее слишком мало - верни 0",
-            },
-            {
-                "role": "user",
-                "content": input_data,
-            },
-        ],
-        temperature=1,
-        max_tokens=1024,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-
-
-def get_gpt_response(input_data):
-    return openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "проведи анализ, чтобы определить, содержит ли сообщение информацию, связанную с криптовалютами или финансами. Для этого просканируй текст сообщения в поисках ключевых слов или фраз, связанных с криптовалютами (например, 'биткоин', 'эфириум', 'доллар') или финансовой тематикой (например, 'инвестиции', 'акции', 'биржа'). Если такие и другие подобные упоминания обнаруживаются, верни значение 1, указывая на наличие связанных с криптовалютами или финансами данных. В противном случае, если в сообщении отсутствуют такие упоминания, верни значение 0, сигнализируя об их отсутствии.",
+                "content": "Проанализируй сообщения и сделай краткую выжимку из 10-15 пунктов которые могут быть наиболее интересны аудитории чата, верни каждое сообщение гиперссылкой (используя ссылку на каждое сообщение) в формате <a href='ссылка'>сообщение</a>, оформи весь ответ красиво",
             },
             {
                 "role": "user",
@@ -55,18 +37,27 @@ def get_gpt_response(input_data):
 
 
 def send_summary():
+    logger.warning("Поток 1 - отправка выжимки запущен!")
     while True:
-        with open("important_messages.txt", "r") as messages_file:
-            message = summarize(messages_file.read())["choices"][0]["message"][
+        with open("messages.csv", mode="r", newline="") as messages_file:
+            csv_reader = csv.reader(messages_file)
+            for row in csv_reader:
+                message, link = row[0], row[1]
+                messages_data += f"Сообщение: {message}, Ссылка: {link}\n"
+
+            if len(messages_data) < 250:
+                return
+
+            summary_message = summarize(messages_data)["choices"][0]["message"][
                 "content"
             ]
 
-            bot.send_message(os.getenv("CHAT_ID"), message, parse_mode="HTML")
+            bot.send_message(os.getenv("CHAT_ID"), summary_message, parse_mode="HTML")
 
         time.sleep(600)
 
-        with open("important_messages.txt", "w") as file:
-            file.write("")
+        with open("messages.csv", mode="w", newline="") as file:
+            pass
 
 
 @bot.message_handler(func=lambda message: True)
@@ -74,21 +65,17 @@ def echo(message):
     message_link = f"https://t.me/c/{str(message.chat.id)[4:]}/{message.id}"
     message_text = message.text
 
-    if len(message_text) < 20:
+    if len(message_text) < 45:
         return
 
-    response = get_gpt_response(message_text)["choices"][0]["message"]["content"]
-
-    if "1" in response:
-        with open("important_messages.txt", "a+") as messages_file:
-            messages_file.write(f"{message_text} {message_link}\n")
-
-        logger.info(f"Сообщение {message_link} помечено как 'Важное' ")
-    else:
-        pass
+    with open("messages.csv", mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([message_text, message_link])
+        logger.info(f"Сообщение {message_link} записано в таблицу ")
 
 
 def main():
+    logger.warning("Поток 2 - основной поток бота запущен!")
     bot.polling()
 
 

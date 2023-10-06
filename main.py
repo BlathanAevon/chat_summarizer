2b7868cf6d458530ad91a39ce0de1f9eff2ac7b3
BlathanAevon <90953033+BlathanAevon@users.noreply.github.com>
2023-10-06 08:17:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
 
-def summarize(input_data):
+def get_gpt_output(input_data, prompt):
     return openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
                 # "content": "Сделай анализ сообщений и выбери из них 5-15 самых важных, составь из этого список с гиперссылками в формате [текст](ссылка), ссылкой должна быть ссылкан а сообщение и также, ссылкой должно быть какое-то важное слово в каждом пунтке, при выборе опирайся на то, что 'важными' имеются ввиду сообщения связаные с криптовалютой, технологиями, финансами и тд.",
-                "content": "Задача заключается в проведении анализа сообщений и выборе наиболее значимых 5-15 из них, после чего необходимо сформировать список, в котором каждый элемент представляет собой гиперссылку в формате [текст](ссылка) извлекается из сообщения и используется в качестве текста ссылки, а ссылка ведет к исходному сообщению; определение важности сообщений основывается на наличии в них ключевых слов и фраз, связанных с криптовалютой, технологиями, финансами и смежными областями, и именно на основе этой аналитики формируется итоговый список. Отправь только список сообщений с ссылками",
+                "content": prompt
             },
             {
                 "role": "user",
@@ -51,11 +51,14 @@ def send_summary():
                 logger.error("Слишком много или мало данных для выжимки!")
                 pass
             else:
-                summary_message = summarize(messages_data)["choices"][0]["message"][
+                prompt = "проанализируй все сообщения, сделай краткую выжимку из 1 - 15 пунктов, верни все сообщения красиво оформленным списком гиперссылок в формате [сообщение](ссылка), начни сообщение с: Вот выжимка самых полезных сообщений:"
+                summary_message = get_gpt_output(messages_data, prompt)["choices"][0]["message"][
                     "content"
                 ]
                 summary_message = summary_message.replace('.', '\\.')
                 summary_message = summary_message.replace('-', '\\-')
+                summary_message = summary_message.replace('(', '\\(')
+                summary_message = summary_message.replace(')', '\\)')
                 bot.send_message(
                     os.getenv("CHAT_ID"), summary_message, parse_mode="MarkdownV2"
                 )
@@ -63,7 +66,7 @@ def send_summary():
 
                 with open("messages.csv", mode="w", newline="") as file:
                     pass
-        tts = 600
+        tts = 1800
         logger.info(f"Жду {tts / 60} минут до следующей выжимки...")
         time.sleep(tts)
 
@@ -73,13 +76,15 @@ def echo(message):
     message_link = f"https://t.me/c/{str(message.chat.id)[4:]}/{message.id}"
     message_text = message.text
 
-    if len(message_text) < 45:
+    if len(message_text) < 60:
         return
 
-    with open("messages.csv", mode="w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([message_text, message_link])
-        logger.info(f"Сообщение {message_link} записано в таблицу ")
+    is_useful = get_gpt_output(message.text, "если сообщение каким либо образом содержит тематику криптовалют, финансов, технологий и подобных тем - верни 1 слово - 1, в противном случае верни 1 слово - 0")
+    if "1" in is_useful:
+        with open("messages.csv", mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([message_text, message_link])
+            logger.info(f"Сообщение {message_link} записано в таблицу ")
 
 
 def main():
